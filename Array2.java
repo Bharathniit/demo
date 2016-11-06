3f463fdf38438b794e4ed58fe10cb1538c9bbb6d
Bharathniit <bharathme1989@yahoo.com>
2016-11-07 01:54:38 +0530

    Add files via upload

---
+++ Array2.java
@@ -0,0 +1,11 @@
+class Array2
+{
+public static void main(String args[])
+{
+int num[]={55,65,75,85,99};
+for(int i=0;i<5;i++)
+{
+System.out.println(num[i]);
+}
+}
+}
---
+++ byteToByte.java
@@ -0,0 +1,10 @@
+public class byteToByte 
+{
+  public static void main(String[] args) 
+  {
+    byte b = 1;
+  
+    Byte Obj = new Byte(b);
+    System.out.println(Obj);
+  }
+}
